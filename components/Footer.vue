93b84641efedc4ff60c0ccf0625a5a0e885b6492
aemanskova <ae.manskova@mail.ru>
2024-03-22 00:07:42 +0300

    test

--- components/Footer.vue
+++ components/Footer.vue
@@ -89,7 +89,11 @@
         <img src="/img/visa.svg" alt="" />
       </div>
     </div>
-    <img class="footer__picture" src="/img/footer-picture.png" alt="" />
+    <img
+      class="footer__picture"
+      src="/test-layout/img/footer-picture.png"
+      alt=""
+    />
   </div>
 </template>
 
